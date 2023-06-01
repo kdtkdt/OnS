d4c104ee12770227d0f97690ea1a37bd83460175
kimyongjin <melonfreecall@hongikit.com>
2023-06-02 00:41:58 +0900

    feat: recruitment 게시판 메인,상세보기 화면 수정 및 추가

--- OnS/src/main/java/com/ons/study/dao/RecruitmentDAO.java
+++ OnS/src/main/java/com/ons/study/dao/RecruitmentDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+
 import com.ons.study.dto.CommentDTO;
 import com.ons.study.dto.RecruitmentDTO;
 @Mapper
